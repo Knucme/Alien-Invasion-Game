7b24ae195bccc1314039431f290ebd6dc4a2247c
Alexis Gonzalez <glep.chair319@passinbox.com>
2025-05-15 12:03:18 -0700

    Included a background image for the game and also added a timer to help with frame rate. Created a settings class to better manage game settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,16 +2,24 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color.
+        self.bg_image = pygame.image.load(self.settings.bg_image_location)
+        self.bg_image = pygame.transform.scale(self.bg_image, (self.screen.get_width(), self.screen.get_height()))
+        
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -20,8 +28,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
+            self.screen.blit(self.bg_image, (0,0))
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
