11b296b74689f1a5051440cd0103618f6477f274
Alexis Gonzalez <glep.chair319@passinbox.com>
2025-05-18 22:18:47 -0700

    Implement ship movement logic for right and left arrow key input

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-        self.settings = Settings()        
+        self.settings = ai_game.settings       
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load(self.settings.ship_image)
@@ -17,6 +17,22 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+        
+        # Movement flags; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
